e9651d382e5b0ea548640650220dec84b95522b9
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2020-09-19 13:21:02 +0200

    Show a polybar in focus mode.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,15 +8,15 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybars -- automatically reload on config changes
 if [ "$MONITOR_RIGHT" != "" ]; then
-  MONITOR=$MONITOR_RIGHT polybar --reload right &
+  MONITOR=$MONITOR_RIGHT polybar --reload ${1:-right} &
 fi
 
 if [ "$MONITOR_LEFT" != "" ]; then
-  MONITOR=$MONITOR_LEFT polybar --reload left &
+  MONITOR=$MONITOR_LEFT polybar --reload ${1:-left} &
 fi
 
 if [ "$MONITOR_LAPTOP" != "" ]; then
-  MONITOR=$MONITOR_LAPTOP polybar --reload left &
+  MONITOR=$MONITOR_LAPTOP polybar --reload ${1:-left} &
 fi
 
 echo "Bars launched..."
