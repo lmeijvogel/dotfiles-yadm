8d0486decce026c957cd0b8b16c7998eb085ac4a
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2020-11-02 10:15:55 +0100

    Revert "Experiment: Set volumes with custom app."
    This experiment had a few downsides:
    * Startup was slower, so quickly stopping music was more difficult,
    * It didn't work correctly when there's a fullscreen application. Didn't
      investigate it further.
    
    This reverts commit ebe855a83fc15ac1f95a77ec5cdd29cb6b91075f.

--- start.sh
+++ start.sh
@@ -3,15 +3,10 @@ ruby ~/.lib/custom-commands/layouts/`hostname`/default.rb
 
 $HOME/bin/mouse left
 
-if [[ -x /usr/bin/nitrogen ]]; then
-  /usr/bin/nitrogen --restore &
-else
-  $HOME/.fehbg
-fi
-
 $HOME/bin/start-polybar
 
-redshift-gtk &
+$HOME/.fehbg
+
 # For some reason, this has to be run *after* start-polybar, otherwise,
 # something in there will reset the caps lock keybinding to normal.
 /usr/bin/setxkbmap -option "caps:swapescape"
