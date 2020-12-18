39876692fb6ab69b333ee2b760ba7c52a71c2919
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2020-12-18 14:56:47 +0100

    Move starting redshift to i3 config so it won't be restarted too many times.

--- start.sh
+++ start.sh
@@ -7,8 +7,6 @@ $HOME/bin/start-polybar
 
 nitrogen --restore
 
-(sleep 20; redshift-gtk) &
-
 # For some reason, this has to be run *after* start-polybar, otherwise,
 # something in there will reset the caps lock keybinding to normal.
 /usr/bin/setxkbmap -option "caps:swapescape"
