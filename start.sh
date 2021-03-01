b6f9028923888ea335a4648b21c771176beac3fd
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2021-03-01 17:10:44 +0100

    Fix start.sh.
    It was started twice and didn't complete.

--- start.sh
+++ start.sh
@@ -3,8 +3,6 @@ ruby ~/.lib/custom-commands/layouts/`hostname`/default.rb
 
 $HOME/bin/mouse left
 
-$HOME/bin/start-polybar
-
 nitrogen --restore &
 
 # The "sleep" is to (hopefully) ensure that compton is started long after all the monitor adjustments
@@ -20,7 +18,10 @@ elif [ -x /usr/bin/compton ]; then
   /usr/bin/compton --config ~/.config/compton/compton.conf &
 fi
 
-killall -q redshift-gtk ; sleep 10; redshift-gtk
+echo Starting polybar >>/tmp/log_start.sh
+$HOME/bin/lib/start-polybar
+
+killall -q redshift-gtk ; sleep 10; redshift-gtk &
 
 # For some reason, this has to be run *after* start-polybar, otherwise,
 # something in there will reset the caps lock keybinding to normal.
