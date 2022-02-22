f697c1e4f9ce30a07f2e5fdab42789867683b4c6
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2022-02-22 15:10:55 +0100

    Always switch caps and escape since I'm no longer using the ErgoDox.

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-$HOME/bin/mouse left
+
+# Button config is now handled by Plasma
+# $HOME/bin/mouse left
 
 # The "sleep" is to (hopefully) ensure that compton is started long after all the monitor adjustments
 if [ -x /usr/sbin/picom ]; then
@@ -15,17 +17,8 @@ $HOME/bin/lib/start-polybar
 # Clear previous xkbmap options
 setxkbmap -option
 
-# Only swap caps and escape if no ErgoDox is connected.
-# I don't know why, but the ErgoDox no longer identifies by name anymore.
-# This id here is what's reported by lsusb now.
-if [[ "$(lsusb | grep 3297:4976)" == "" ]]; then
-  # For some reason, this has to be run *after* start-polybar, otherwise,
-  # something in there will reset the caps lock keybinding to normal.
-
-  /usr/bin/setxkbmap -option "caps:swapescape"
-else
-  /usr/bin/setxkbmap -option
-fi
+# Swap caps lock and escape
+/usr/bin/setxkbmap -option "caps:swapescape"
 
 # Make right alt the compose key
 setxkbmap -option 'compose:ralt'
