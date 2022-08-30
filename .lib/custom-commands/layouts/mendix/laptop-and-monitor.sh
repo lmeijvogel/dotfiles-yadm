e8863410190fe216c3453f1ee8b2ccd1c1b1a69b
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2022-08-30 09:07:25 +0200

    Add monitor layout for work setup.

---
+++ .lib/custom-commands/layouts/mendix/laptop-and-monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off --output DP-2-1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP-2-2 --off --output DP-2-3 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off
