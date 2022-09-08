e0ccc9b922461129e4f06585656b36c285b60058
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2022-09-08 17:09:18 +0200

    Fix reloading of polybar: Do not limit killed processes to current user.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env /bin/bash
 
 # Terminate already running bar instances
 killall -q -w polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
 MONITORS=`xrandr | grep " connected" | awk '{ print $1; }'`
 
