503c67348c4c94ce4bb79ee5ef94df7f33bab72e
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2022-09-12 16:43:18 +0200

    Remove log statements.

--- bin/lib/position-zoom-meetings.sh
+++ bin/lib/position-zoom-meetings.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-echo 1 $1 >/tmp/pos_zooms
-echo 2 $2 >/tmp/pos_zooms
 i3-msg '[title="Zoom Meeting"]' move to workspace "$1"
 i3-msg '[title="Zoom$"]' move to workspace "$2"
 i3-msg workspace "$2"
