b05501d2ca1dd333099a47f415aad53d78fac1fa
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2022-10-03 08:48:59 +0200

    Update zoom screen mover to also put workspaces on the correct display.

--- bin/lib/position-zoom-meetings.sh
+++ bin/lib/position-zoom-meetings.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 i3-msg '[title="Zoom Meeting"]' move to workspace "$1"
-i3-msg '[title="Zoom$"]' move to workspace "$2"
-i3-msg workspace "$2"
+i3-msg '[title="Zoom$"]' move to workspace "$3"
+
+i3-msg workspace "$3"
+i3-msg move workspace to output "$4"
 i3-msg workspace "$1"
+i3-msg move workspace to output "$2"
