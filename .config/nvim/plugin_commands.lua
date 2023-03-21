57db4b433fdfbf9fda8c796b83eb22fcad887bc7
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2023-03-21 20:12:05 +0100

    Remove C-k keymap since it interferes with split switching.

--- .config/nvim/plugin_commands.lua
+++ .config/nvim/plugin_commands.lua
@@ -103,7 +103,6 @@ local on_attach = function(client, bufnr)
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
