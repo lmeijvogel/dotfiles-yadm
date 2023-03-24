24df30347f6b2a07c4638dfed373dc955a44af29
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2023-03-24 09:05:49 +0100

    Do not show blank chars and context-start in vim indent_blankline...
    It looks too much line LSP diagnostics.

--- .config/nvim/lua/plugin_commands.lua
+++ .config/nvim/lua/plugin_commands.lua
@@ -30,13 +30,11 @@ require("nvim-treesitter.configs").setup({
 })
 
 vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
-vim.opt.listchars:append "eol:↴"
 
 require("indent_blankline").setup {
     space_char_blankline = " ",
     show_current_context = true,
-    show_current_context_start = true,
+    show_current_context_start = false, -- The start marker resembles an LSP diagnostic
 }
 
-require('colorizer').setup()
+require('colorizer').setup({})
