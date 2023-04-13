6c6e1c49695d8081b4a8a6d3974cca83ef90ebd9
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2023-04-13 22:03:01 +0200

    Improve nvim-lsp mappings a bit more.

--- .config/nvim/lua/language_server/init.lua
+++ .config/nvim/lua/language_server/init.lua
@@ -6,6 +6,12 @@ require('mason-lspconfig').setup {
   ensure_installed = { 'tsserver', 'eslint', 'cssls' }
 }
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
@@ -75,34 +81,36 @@ cmp.setup({
     completion = cmp.config.window.bordered(),
     documentation = cmp.config.window.bordered(),
   },
+  -- behaviors: `SelectBehavior`: Highlights the entry, but don't put it in the buffer yet
+  --            `InsertBehavior`: Highlight the entry and put it in the buffer.
   mapping = cmp.mapping.preset.insert({
-    ['<C-k>'] = cmp.mapping.select_prev_item(),
-    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.SelectBehavior } ),
+    ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.SelectBehavior } ),
+    -- By default, <Up> and <Down> have { behavior = types.cmp.SelectBehavior.SelectBehavior },
+    -- which highlights the items but does not put the text in the buffer.
+    -- I don't know how to select the highlighted items in that case, so here without the `SelectBehavior`
+    -- ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.InsertBehavior }),
+    -- ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.InsertBehavior }),
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping({
       i = function(fallback)
-        if cmp.visible() and cmp.get_active_entry() then
-          cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+        if cmp.visible() and cmp.get_selected_entry() then
+          cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = false })
         else
           fallback()
         end
       end,
-      s = cmp.mapping.confirm({ select = true }),
+      s = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
       c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
     }),
     ['<Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
-            cmp.select_next_item()
-        else
-            fallback()
-        end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-            cmp.select_prev_item()
+            cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
+        elseif has_words_before() then
+            cmp.complete()
         else
             fallback()
         end
