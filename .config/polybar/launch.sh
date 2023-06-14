ee66263ec8488fdc88476eab4329bcb12d684ba1
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2023-06-14 10:49:06 +0200

    Simplify polybar launch scripts.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,9 +3,6 @@
 # Terminate already running bar instances
 killall -q -w polybar
 
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
-
 MONITORS=`xrandr | grep " connected" | awk '{ print $1; }'`
 
 for monitor in $MONITORS
