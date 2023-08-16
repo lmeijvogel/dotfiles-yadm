3cb562a65bb9986879ead9a191a9bd683fb6c597
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2023-08-16 16:13:42 +0200

    Truncate polybar branch name if it gets too long.

--- bin/polybar/current-dev-branch.rb
+++ bin/polybar/current-dev-branch.rb
@@ -6,7 +6,14 @@ def main
   type = ARGV[0]
   dir = ARGV[1]
 
+  if type != "name" && type != "icon"
+    $stderr.puts usage
+    puts ""
+    exit
+  end
+
   if dir.nil?
+    $stderr.puts usage
     puts ""
     exit
   end
@@ -31,7 +38,31 @@ def render_branch_name(symbolic_ref)
   # which is nice if we're rebasing.
   exit if symbolic_ref == ""
 
-  puts symbolic_ref
+  story_name, description = split(symbolic_ref)
+
+  desired_length = 25
+
+  # I want to save at least one char and truncating adds 3, so 4.
+  if description.length - 4 > desired_length
+    puts truncate(symbolic_ref, desired_length)
+  else
+    puts symbolic_ref
+  end
+end
+
+def truncate(ref, desired_length)
+  story_name, description = split(ref)
+
+  return story_name + "-" + description[0..desired_length/2] + "..." + description[-desired_length/2..-1]
+end
+
+def split(ref)
+  parts = %r[(\w+/\d+)-(.*)].match(ref)
+
+  story_name = parts[1]
+  description = parts[2]
+
+  [story_name, description]
 end
 
 def render_status_icon(symbolic_ref)
@@ -44,4 +75,11 @@ def render_status_icon(symbolic_ref)
   end
 end
 
+def usage
+  <<~USAGE
+    Usage:
+
+      #{$0} (name|icon) <directory>
+  USAGE
+end
 main
