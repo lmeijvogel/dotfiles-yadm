867378b3c00fb9825700b1637f7445551ba3e34b
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2024-03-27 10:25:22 +0100

    Add neogit

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -20,9 +20,24 @@ return {
   {
     "tpope/vim-fugitive",
     config = function()
-      map('n', '<leader>gs', '<cmd>Git<CR>', { desc = "Status" })
       map('n', '<leader>ga', '<cmd>Git add %<CR>', { desc = "Add current file" })
       vim.keymap.set('n', '<leader>gb', '<cmd>Git blame<CR>', { desc = "Git blame" })
+    end
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",  -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua",              -- optional
+    },
+    config = function()
+      require('neogit').setup()
+
+      map('n', '<leader>gs', '<cmd>Neogit<CR>', { desc = "Status" })
     end
   },
   "junegunn/fzf",
