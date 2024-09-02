4d90081c51d2a89b132211534db81e308acdf603
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2024-09-02 17:04:34 +0200

    Fix Neoformat errors

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -166,7 +166,7 @@ return {
       vim.cmd [[
         augroup fmt
           autocmd!
-          autocmd BufWritePre * undojoin | Neoformat
+          autocmd BufWritePre * try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
         augroup END
     ]]
     end
