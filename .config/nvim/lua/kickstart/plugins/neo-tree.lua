fe261001a52efab1b6da53f3d73bebe097534636
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2024-10-24 17:00:36 +0200

    Add shortcut shift-p in neo-tree to go up one directory

--- .config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ .config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -29,5 +29,13 @@ return {
         },
       },
     },
+    window = {
+      mappings = {
+        ['P'] = function(state)
+          local node = state.tree:get_node()
+          require('neo-tree.ui.renderer').focus_node(state, node:get_parent_id())
+        end,
+      },
+    },
   },
 }
