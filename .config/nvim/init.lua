4924fc6657978dd8d5c81864b8ccdd42115fbf2c
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2024-12-13 10:33:27 +0100

    Add keyboard resizing for neovide

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -81,6 +81,11 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0
 
   vim.g.neovide_hide_mouse_when_typing = true
+
+  vim.api.nvim_set_keymap('n', '<C-+>', ':lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>', { silent = true })
+  vim.api.nvim_set_keymap('n', '<C-=>', ':lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>', { silent = true })
+  vim.api.nvim_set_keymap('n', '<C-->', ':lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>', { silent = true })
+  vim.api.nvim_set_keymap('n', '<C-0>', ':lua vim.g.neovide_scale_factor = 1<CR>', { silent = true })
 end
 
 -- Add aliases to catch typos
