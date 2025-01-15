8f993af02a62dbb1a5e5c586bb333418bac23162
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2025-01-15 11:25:25 +0100

    Add function to grep note contents

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -154,6 +154,29 @@ return {
       vim.keymap.set('n', '<leader>klJ',
         function() require("zk.commands").get("ZkNotes")({ tags = { "journal" }, sort = { "created-" } }) end,
         { desc = "List [J]ournal entries (all)" })
+
+      local function grep_notes(opts)
+        local collection = {}
+        local list_opts = { select = { "title", "path", "absPath" } }
+        require("zk.api").list(vim.env.ZK_NOTEBOOK_DIR, list_opts, function(_, notes)
+          for _, note in ipairs(notes) do
+            collection[note.absPath] = note.title or note.path
+          end
+        end)
+        local options = vim.tbl_deep_extend("force", {
+          prompt_title = "Notes",
+          search_dirs = { vim.env.ZK_NOTEBOOK_DIR },
+          disable_coordinates = true,
+          path_display = function(_, path)
+            return collection[path]
+          end,
+        }, opts or {})
+        require("telescope.builtin").live_grep(options)
+      end
+
+      vim.api.nvim_create_user_command("ZkGrep", grep_notes, {})
+
+      vim.keymap.set('n', '<leader>kg', function() grep_notes() end, { desc = "[g]rep notes" })
     end,
   },
   {
