2acab2fb8b2da5bbb66458be004c347e2fecf97e
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2025-05-26 17:13:56 +0200

    Install nvim onedarkpro theme and set it as the default

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -416,9 +416,24 @@ require('lazy').setup({
       },
     },
   },
-  { 'Bilal2453/luvit-meta',     lazy = true },
+  { 'Bilal2453/luvit-meta', lazy = true },
 
+  {
+    'olimorris/onedarkpro.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      local colors = require('onedarkpro.helpers').get_colors()
 
+      require('onedarkpro').setup {
+        highlights = {
+          StatusLine = { fg = colors.bg, bg = colors.fg },
+          StatusLineNC = { fg = colors.bg, bg = colors.fg },
+        },
+      }
+
+      vim.cmd.colorscheme 'onedark'
+    end,
+  },
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
@@ -430,7 +445,6 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-day'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
